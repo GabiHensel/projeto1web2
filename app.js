48e14d3916f74f6e6a5570ef610476e117edfb3e
Gabriela Hensel Ciudrowski <106684514+GabiHensel@users.noreply.github.com>
2024-04-19 21:24:25 -0300

    final front

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.get("/",function(req,res){
     res.sendFile(path.join(__dirname + '/views/loginPage.html'));
 });
 
+app.get("/Autor",function(req,res){
+    res.sendFile(path.join(__dirname + '/views/inicioAutor.html'));
+});
+
+app.get("/Admin",function(req,res){
+    res.sendFile(path.join(__dirname + '/views/inicioAdmin.html'));
+});
+
+app.get("/Avaliador",function(req,res){
+    res.sendFile(path.join(__dirname + '/views/inicioAvaliador.html'));
+});
+
 app.listen(3000,function(){
     console.log("teste")
 });
